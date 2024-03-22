87942898dc2ff865acbea0f5a6a9b217b47d8b0d
Seth Anzai <sethanzai@gmail.com>
2024-03-22 11:37:26 +0900

    features added

--- js/main.js
+++ js/main.js
@@ -12,18 +12,30 @@ window.onscroll = function(ev) {
 };
 
 function scrollToSection(section) {
-    document.getElementById(section).scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+    document.getElementById(section).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 }
 
 function scrollButtonClicked() {
     var home = document.getElementById('home');
     var about = document.getElementById('about-app');
     var sections = document.getElementsByTagName('section');
-    console.log(sections.length)
+    var nextSection = "";
+    var nextSectionValue = 0;
     for (var i=0; i < sections.length; i++) {
-        console.log(sections[i].getBoundingClientRect().top)
-        // console.log(section.getBoundingClientRect().top)
+        const sectionValue = sections[i].getBoundingClientRect().top
+        console.log(sections[i].id, sectionValue);
+        if (sectionValue > 0) {
+            if (nextSectionValue == 0) {
+                nextSection = sections[i].id;
+                nextSectionValue = sectionValue;
+            }
+            else if (nextSectionValue > sectionValue) {
+                nextSection = sections[i].id;
+                nextSectionValue = sectionValue;
+            }
+        }
     }
+    scrollToSection(nextSection);
     // console.log(Math.round(window.scrollY), about.getBoundingClientRect().top, home.getBoundingClientRect().top)
     // about.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 }
